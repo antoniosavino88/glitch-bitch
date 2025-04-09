755d5ccd3c1b610768742bd832426346d221d24c
Luciana Tudela <luciana.tudela@hotmail.com>
2025-04-09 17:50:49 +0200

    form autenticazione e form creazione degli articoli

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -36,5 +36,36 @@
                 <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
         </div>
+        @auth
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('article.create') }}">Inserisci articolo</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    {{ Auth::user()->name }}
+                </a>
+                <ul class="dropdown-menu">
+                    <a class="dropdown-item" href="#"
+                        onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">Logout</a>
+                    <form action="{{ route('logout') }}" method="POST" id="form-logout" class="d-none">
+                        @csrf
+                    </form>
+                </ul>
+            </li>
+        @endauth
+        @guest
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Benvenuto Ospite
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="{{ route('register') }}">Registrati</a></li>
+                    <li><a class="dropdown-item" href="{{ route('login') }}">Accedi</a></li>
+                </ul>
+            </li>
+        @endguest
+        </ul>
     </div>
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,11 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
+                @if (session('message'))
+                    <div class="alert alert-success">
+                        {{ session('message') }}
+                    </div>
+                @endif
                 <h1 class="text-center p-5">Glitch&Bitch</h1>
             </div>
         </div>
